b459da568de47f34913f8dda8522074dc909df6f
Nathan Darmawan <nathandarmawan@gmail.com>
2014-10-09 17:20:54 +0800

    re-commented "Unwise Assassin"

--- hero.js
+++ hero.js
@@ -59,14 +59,14 @@
 
 // // The "Unwise Assassin"
 // // This hero will attempt to kill the closest enemy hero. No matter what.
- var move = function(gameData, helpers) {
-   var myHero = gameData.activeHero;
-   if (myHero.health < 30) {
-     return helpers.findNearestHealthWell(gameData);
-   } else {
-     return helpers.findNearestEnemy(gameData);
-   }
-};
+// var move = function(gameData, helpers) {
+//   var myHero = gameData.activeHero;
+//   if (myHero.health < 30) {
+//     return helpers.findNearestHealthWell(gameData);
+//   } else {
+//     return helpers.findNearestEnemy(gameData);
+//   }
+// };
 
 // // The "Careful Assassin"
 // // This hero will attempt to kill the closest weaker enemy hero.
